5edeb81951855b6977b16628f7d5e746d4dc6a73
Neil <h66r10@gmail.com>
2020-12-07 05:55:00 +0100

    Basic day 7 setup

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs;
+use std::time::Instant;
+
+fn part1(questions:Vec<String>) ->usize{
+
+}
+fn part2(questions:Vec<String>)->usize{
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input/test.txt")
+        .expect("Something went wrong reading the file");
+    let lines = input.lines();
+    let mut questions: Vec<String> = vec![];
+    for line in lines {
+        questions.push(line.parse::<String>().expect("Ouf that's not a string !"))
+    }
+    println!("Running part1");
+    let now = Instant::now();
+    println!("Found {}", part1(questions.clone()));
+    println!("Took {}us", now.elapsed().as_micros());
+
+    println!("Running part2");
+    let now = Instant::now();
+    println!("Found {}", part2(questions.clone()));
+    println!("Took {}us", now.elapsed().as_micros());
 }
